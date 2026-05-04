2281c4da844d85b90a3df4141d1cf6781a06865d
Sharon <sharon.saenz@utp.ac.pa>
2026-05-04 02:28:11 -0500

    Fix headless selenium

--- test_tienda.py
+++ test_tienda.py
@@ -6,9 +6,10 @@ from login_page import LoginPage
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     yield driver
@@ -19,15 +20,11 @@ def test_login_exitoso(driver):
     login = LoginPage(driver)
 
     login.login("standard_user", "secret_sauce")
-
-    
     assert "inventory" in driver.current_url
-
 
 def test_login_fallido(driver):
     driver.get("https://www.saucedemo.com/")
     login = LoginPage(driver)
 
     login.login("locked_out_user", "secret_sauce")
-
     assert "locked out" in login.obtener_error()
