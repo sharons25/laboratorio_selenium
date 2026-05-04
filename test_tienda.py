482c062066d49468f980a738c06e3279470cce0c
Sharon <sharon.saenz@utp.ac.pa>
2026-05-04 02:32:11 -0500

    Fix driver setup

--- test_tienda.py
+++ test_tienda.py
@@ -1,7 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from login_page import LoginPage
+
 
 @pytest.fixture
 def driver():
@@ -11,9 +13,14 @@ def driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(
+        service=Service('/usr/bin/chromedriver'),
+        options=options
+    )
+
     yield driver
     driver.quit()
+
 
 def test_login_exitoso(driver):
     driver.get("https://www.saucedemo.com/")
@@ -21,6 +28,7 @@ def test_login_exitoso(driver):
 
     login.login("standard_user", "secret_sauce")
     assert "inventory" in driver.current_url
+
 
 def test_login_fallido(driver):
     driver.get("https://www.saucedemo.com/")
